55301f2f832a40a42c2cc4744758c811efecf6c8
Cédric Bonhomme <cedric@cedricbonhomme.org>
2014-10-05 12:29:06 +0200

    List all entries...

--- pwdmanager.hs
+++ pwdmanager.hs
@@ -4,6 +4,7 @@ import System.Environment
 
 dispatch :: String -> [String] -> IO ()
 dispatch "add" = add
+dispatch "list" = list
 
 main = do
     (command:argList) <- getArgs
@@ -12,3 +13,11 @@ main = do
 add :: [String] -> IO ()
 add [fileName , service, userName, password] =
     appendFile fileName (service ++ ":" ++ userName ++ ":" ++ password ++ "\n")
+
+list :: [String] -> IO ()
+list [fileName] = do
+    contents <- readFile fileName
+    let entries = lines contents
+        numberedEntries = zipWith (\n line -> show n ++ " - "  ++ line)
+                            [0..] entries
+    putStr $ unlines numberedEntries
